c9102f0a78f1184b1048890481eb67fd48e1a380
mito99 <mito@live.jp>
2024-10-01 08:46:26 +0900

    Refactor: DeleteCommandをCleanupCommandとSetupCommandに分割

--- command/src/main/java/com/github/mito99/javacallgraph/Main.java
+++ command/src/main/java/com/github/mito99/javacallgraph/Main.java
@@ -2,20 +2,16 @@ package com.github.mito99.javacallgraph;
 
 import java.nio.file.Path;
 import java.util.concurrent.Callable;
-
 import com.github.mito99.javacallgraph.bytecode.MtClassPool;
 import com.github.mito99.javacallgraph.bytecode.MtConfig;
 import com.github.mito99.javacallgraph.database.GraphDbRepository;
 import com.github.mito99.javacallgraph.database.GraphDbSession;
-
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "javacallgraph", mixinStandardHelpOptions = true, description = "Java Call Graph", subcommands = {
-    RegisterCommand.class,
-    DeleteCommand.class,
-})
+@Command(name = "javacallgraph", mixinStandardHelpOptions = true, description = "Java Call Graph",
+    subcommands = {RegisterCommand.class, CleanupCommand.class, SetupCommand.class})
 public class Main implements Runnable {
 
   public static void main(String[] args) {
@@ -25,22 +21,23 @@ public class Main implements Runnable {
 
   @Override
   public void run() {
-    System.out.println("Use one of the subcommands: register or delete.");
+    System.out.println("Use one of the subcommands: register or cleanup or setup.");
   }
 }
 
+
 @Command(name = "register", description = "Register module")
 class RegisterCommand implements Callable<Integer> {
-  @Option(names = { "-m", "--module" }, description = "Module name", required = true)
+  @Option(names = {"-m", "--module"}, description = "Module name", required = true)
   private String moduleName;
 
-  @Option(names = { "-t", "--type" }, description = "Module type", required = true)
+  @Option(names = {"-t", "--type"}, description = "Module type", required = true)
   private String moduleType;
 
-  @Option(names = { "-d", "--directory" }, description = "Directory path", required = true)
+  @Option(names = {"-d", "--directory"}, description = "Directory path", required = true)
   private Path directoryPath;
 
-  @Option(names = { "-i", "--include" }, description = "Include classes (正規表現で指定)")
+  @Option(names = {"-i", "--include"}, description = "Include classes (正規表現で指定)")
   private String includeClassesRegex;
 
   @Override
@@ -50,7 +47,6 @@ class RegisterCommand implements Callable<Integer> {
     try (var session = GraphDbSession.start()) {
       final var module = MtClassPool.getModule(directoryPath, moduleName, moduleType);
       final var reqpos = new GraphDbRepository(session);
-      reqpos.createIndexes();
       reqpos.registerModule(module);
       return 0;
     } catch (Exception e) {
@@ -61,8 +57,9 @@ class RegisterCommand implements Callable<Integer> {
   }
 }
 
-@Command(name = "delete", description = "Delete all nodes")
-class DeleteCommand implements Callable<Integer> {
+
+@Command(name = "cleanup", description = "Cleanup database")
+class CleanupCommand implements Callable<Integer> {
 
   @Override
   public Integer call() {
@@ -70,6 +67,23 @@ class DeleteCommand implements Callable<Integer> {
       final var reqpos = new GraphDbRepository(session);
       reqpos.deleteAllNodes();
       reqpos.dropIndexes();
+      return 0;
+    } catch (Exception e) {
+      System.err.println("An error occurred: " + e.getMessage());
+      return 1;
+    }
+  }
+}
+
+
+@Command(name = "setup", description = "Setup database")
+class SetupCommand implements Callable<Integer> {
+
+  @Override
+  public Integer call() {
+    try (var session = GraphDbSession.start()) {
+      final var repo = new GraphDbRepository(session);
+      repo.createIndexes();
       return 0;
     } catch (Exception e) {
       System.err.println("An error occurred: " + e.getMessage());
